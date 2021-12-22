55f02873a63e2ea4bcc74102c210bf7548515e00
Mohammad Jaha <miro.tune@gmail.com>
2021-12-22 07:26:13 +0000

    Uploading Assignment

--- GETPeople/PeopleViewController.swift
+++ GETPeople/PeopleViewController.swift
@@ -16,63 +16,27 @@ class PeopleViewController: UIViewController {
     @IBOutlet weak var previousButton: UIButton!
     @IBOutlet weak var pageNumberLabel: UILabel!
     
+    let aPIConnection = APIConnection()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         check()
-        gettingDataFromAPI()
+        aPIConnection.dataDelegate = self
+        aPIConnection.gettingPeopleFromAPI(pageNumber: pageNumber)
         mainTableView.dataSource = self
-    }
-    
-    func gettingDataFromAPI() {
-        peopleList.removeAll()
-        // specify the url that we will be sending the GET Request to
-        let url = URL(string: "https://swapi.dev/api/people/?page=\(pageNumber)&format=json")
-        // create a URLSession to handle the request taskscopy
-        let session = URLSession.shared
-        // create a "data task" to make the request and run the completion handler
-        let task = session.dataTask(with: url!, completionHandler: {
-            // see: Swift closure expression syntax
-            data, response, error in
-            do {
-                // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
-                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
-                    if let results = jsonResult["results"] {
-                        // coercing the results object as an NSArray and then storing that in resultsArray
-                        let resultsArray = results as! NSArray
-                        // now we can run NSArray methods like count and firstObject
-                        for result in resultsArray {
-                            if let data = result as? NSDictionary {
-                                if let result = data["name"] {
-                                    let name = result as! String
-                                    self.peopleList.append(name)
-                                }
-                            }
-                        }
-                        DispatchQueue.main.async {
-                            self.mainTableView.reloadData()
-                        }
-                    }
-                }
-            } catch {
-                print(error)
-            }
-        })
-        // execute the task and wait for the response before
-        // running the completion handler. This is async!
-        task.resume()
     }
 
     @IBAction func nextButtonPressed(_ sender: UIButton) {
         pageNumber += 1
         pageNumberLabel.text = "Page: \(pageNumber)/9"
         check()
-        gettingDataFromAPI()
+        aPIConnection.gettingPeopleFromAPI(pageNumber: pageNumber)
     }
     @IBAction func previousButtonPressed(_ sender: UIButton) {
         pageNumber -= 1
         pageNumberLabel.text = "Page: \(pageNumber)/9"
         check()
-        gettingDataFromAPI()
+        aPIConnection.gettingPeopleFromAPI(pageNumber: pageNumber)
     }
     func check() {
         if pageNumber == 9 {
@@ -98,6 +62,19 @@ extension PeopleViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = peopleList[indexPath.row]
         return cell
+    }
+    
+    
+}
+
+extension PeopleViewController: DataDelegate {
+    func gettingPeople(peopleList: [String]) {
+        self.peopleList = peopleList
+        mainTableView.reloadData()
+    }
+    
+    func gettingFilms(filmsList: [String]) {
+        
     }
     
     
